6fa94b082ad7e4e20a3109e3f1d0cb7368f4fe01
Akash T B <akashtb21@gmail.com>
2024-10-17 12:04:28 +0530

    protected routes are all set

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { Provider } from 'react-redux'; 
-import { store } from './Store.jsx';
+import { Provider } from 'react-redux';
+import { store, persistor } from './Store.jsx'; // Import the persistor
+import { PersistGate } from 'redux-persist/integration/react'; // Import PersistGate
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}> 
-      <App />
+    <Provider store={store}>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}> {/* Use PersistGate */}
+        <App />
+      </PersistGate>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 );
--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -1,13 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit"
-import { apiSlice } from "./api/apiSlice"
-import authReducer from './features/auth/AuthSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+import authReducer from './features/auth/AuthSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { combineReducers } from 'redux';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        [apiSlice.reducerPath]: apiSlice.reducer,
-        auth: authReducer
-    },
-    middleware: getDefaultMiddleware =>
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(apiSlice.middleware),
-    devTools: true
-})
+    devTools: true,
+});
+
+export const persistor = persistStore(store);
